ecf018af20478945ef368fac263c0f757c4f34a2
afewvowels <keithbsmith87@gmail.com>
2018-10-31 14:28:14 -0400

    Initial commit

---
+++ Lab_07/BookSale.vb
@@ -0,0 +1,88 @@
+﻿'Program:		Lab #7
+'Programmer:	Keith Smith
+'Date:			31 October 2018
+'Description:	Describes the BookSale class to be used in the main SalesForm.vb
+
+Option Explicit On
+Option Strict On
+
+Public Class BookSale
+    ' Define variables for properties
+    Private QuantityInteger As Integer
+    Private PriceDecimal, ExtendedPriceDecimal As Decimal
+
+    ' Define Properties
+    Private Shared AccumulatorSalesTotalDecimal As Decimal
+
+    ' Title is an auto-implemented property
+    Public Property Title As String
+
+    ' Quantity and Price use logic to calculate values
+    Public Property Quantity As Integer
+        Get
+            Return QuantityInteger
+        End Get
+        Set(value As Integer)
+            ' Validate that at least 1 book is indicated
+            If (value < 1) Then
+                Throw New ArgumentOutOfRangeException("Quantity must be greater than 0")
+            End If
+
+            QuantityInteger = value
+        End Set
+    End Property
+    Public Property Price As Decimal
+        Get
+            Return PriceDecimal
+        End Get
+        Set(value As Decimal)
+            ' Validate that positive price is entered
+            If (value <= 0) Then
+                Throw New ArgumentOutOfRangeException("Price must be greater than $0.00")
+            End If
+
+            PriceDecimal = value
+        End Set
+    End Property
+    ' Use read-only since ExtendedPrice is a calculated value
+    Public ReadOnly Property ExtendedPrice As Decimal
+        Get
+            Return ExtendedPriceDecimal
+        End Get
+    End Property
+
+    ' Use shared to share same variable across all created BookSale objects
+    Public Shared ReadOnly Property AccumulatorSalesTotal As Decimal
+        Get
+            Return AccumulatorSalesTotalDecimal
+        End Get
+    End Property
+
+    ' Define constructors
+    Sub New(ByVal _TitleString As String, ByVal _QuantityInteger As Integer, ByVal _PriceDecimal As Decimal)
+        ' Use property get/set methods that contain validation logic
+        ' Prevents need to validate before passing values to object
+        ' Use property methods, not assigning directly to variables
+        Title = _TitleString
+        Quantity = _QuantityInteger
+        Price = _PriceDecimal
+
+        ' Update calculated extended price value
+        CalculateExtendedPrice()
+
+        ' Update total sales accumulator
+        AddToAccumulator()
+    End Sub
+
+
+    ' Define methods
+
+    ' Calculate extended price for display and use
+    Private Sub CalculateExtendedPrice()
+        ExtendedPriceDecimal = Price * Quantity
+    End Sub
+
+    Private Sub AddToAccumulator()
+        AccumulatorSalesTotalDecimal += ExtendedPrice
+    End Sub
+End Class
---
+++ Lab_07/SalesForm.vb
@@ -0,0 +1,73 @@
+﻿'Program:		Lab #7
+'Programmer:	Keith Smith
+'Date:			31 October 2018
+'Description:	Calculate sales price using the BookSale class.
+'				Instantiate TheBookSale as a new object of the BookSale class.
+
+Option Explicit On
+Option Strict On
+
+Public Class SalesForm
+    ' Declare the new object.
+    Dim TheBookSale As BookSale
+
+
+    Private Sub CalculateSaleToolStripMenuItem_Click(ByVal sender As System.Object,
+     ByVal e As System.EventArgs) Handles CalculateSaleToolStripMenuItem.Click
+        ' Calculate the extended price for the sale.
+        Dim TempQuantityInteger As Integer
+        Dim TempPriceDecimal As Decimal
+
+        Try
+            ' Parse text fields to temporary variables
+            TempQuantityInteger = Convert.ToInt32(QuantityTextBox.Text)
+            TempPriceDecimal = Convert.ToDecimal(PriceTextBox.Text)
+
+            ' Instantiate new object with overloaded constructor
+            TheBookSale = New BookSale(TitleTextBox.Text, TempQuantityInteger, TempPriceDecimal)
+
+            ' Display the calculated extended price result
+            ExtendedPriceTextBox.Text = TheBookSale.ExtendedPrice.ToString("c")
+
+            ' Display total sales accumulated
+            TotalSalesTextBox.Text = BookSale.AccumulatorSalesTotal.ToString("c")
+        Catch ex As FormatException
+            MessageBox.Show("Must use numeric values for quantity and price",
+                            "Format Exception",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Exclamation)
+        Catch ex As ArgumentOutOfRangeException
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub ClearToolStripMenuItem_Click(ByVal sender As System.Object,
+      ByVal e As System.EventArgs) Handles ClearToolStripMenuItem.Click
+        ' Clear the screen controls.
+
+        QuantityTextBox.Clear()
+        PriceTextBox.Clear()
+        ExtendedPriceTextBox.Clear()
+        With TitleTextBox
+            .Clear()
+            .Focus()
+        End With
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object,
+      ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        ' Exit the program.
+
+        Me.Close()
+    End Sub
+
+    Private Sub SummaryToolStripMenuItem_Click(ByVal sender As System.Object,
+      ByVal e As System.EventArgs) Handles SummaryToolStripMenuItem.Click
+        ' Display the sales summary information.
+        MessageBox.Show("Total sales: " & BookSale.AccumulatorSalesTotal.ToString("c"),
+                        "Total Sales Amount",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation)
+    End Sub
+End Class
